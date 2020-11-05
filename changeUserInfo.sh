3df45756d9c0e168f9d59cce7b3efdf134b564c1
yll <lulu.yang@casstime.com>
2020-11-05 09:24:44 +0800

    add changeUserInfo.sh

---
+++ changeUserInfo.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+git filter-branch --env-filter '
+OLD_EMAIL="yanglulu@wezhuiyi.com"
+CORRECT_NAME="yanglulu"
+CORRECT_EMAIL="yanglu_u@126.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
